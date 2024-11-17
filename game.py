5bcc2cd7864a1d59c3db8556c43a8319a952684c
David Doherty <froztydavie@Davids-MacBook-Air-3.local>
2024-11-17 09:01:28 -0800

    grey circles

--- game.py
+++ game.py
@@ -4,23 +4,13 @@ pygame.init()
 #confirm screen size
 screen = pygame.display.set_mode([500, 500])
 
-#define colors
-red = (255, 59, 48)
-orange = (255, 149, 0)
-yellow = (255, 204, 0)
-green = (76, 217, 100)
-blue = (0, 122, 255)
+#define colors & radius
+dark_grey = (50, 50, 50)
+radius = 50
 
-#positions 
-positions = [
-    (150, 150), #red circle
-    (350, 150), #organge circle
-    (250, 250), #yellow circle
-    (150, 350), #green circle
-    (350, 350) #blue circle
-]
-
-radius = 50 #circle radius
+#spacing
+spacing = 150
+offset = 75
 
 #game loop
 running = True
@@ -32,12 +22,12 @@ while running:
 
     #clear the screen
     screen.fill((255, 255, 255))
-    #draw circle
-    pygame.draw.circle(screen, red, positions[0], radius)
-    pygame.draw.circle(screen, orange, positions[1], radius)
-    pygame.draw.circle(screen, yellow, positions[2], radius)
-    pygame.draw.circle(screen, green, positions[3], radius)
-    pygame.draw.circle(screen, blue, positions[4], radius)
+    #nested loops to draw the circles
+    for row in range(3):
+        for col in range(3):
+            x = offset + col * spacing
+            y = offset + row * spacing
+            pygame.draw.circle(screen, dark_grey, (x, y), radius)
     # update display
     pygame.display.flip()
 
